3dd9b244aef64b76bf5bcf0b56da341be984d294
Rumi <lumee.yi@gmail.com>
2023-07-01 13:03:56 +0900

    클릭시 배경 연두색으로 바뀜 + / server는 지도 열림

---
+++ Evcharger/src/routes/+page.svelte
@@ -0,0 +1,113 @@
+<script>
+    import { onMount } from "svelte";
+
+    let isClickArr_charger = [false, false, false];
+    let isClickArr_charger_DC = [false];
+    let isClickArr_wayfast = [false];
+    let isClickArr_wayfull = [false];
+</script>
+
+<div class="container">
+    <div class="title">타입</div>
+    {#each isClickArr_charger as elm, i}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div
+            class="type {elm ? 'clicked' : ''}"
+            on:click={() => (isClickArr_charger[i] = !isClickArr_charger[i])}
+        >
+            <img
+                src="/img/charge{i}.png"
+                width="200"
+                height="200"
+                alt="charge{i}"
+            /><br />
+            <span>AC/DC</span>
+        </div>
+    {/each}
+    {#each isClickArr_charger_DC as elm, i}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div
+            class="type {elm ? 'clicked' : ''}"
+            on:click={() => (isClickArr_charger_DC[i] = !isClickArr_charger_DC[i])}
+        >
+            <img
+                src="/img/charge{i+3}.png"
+                width="200"
+                height="200"
+                alt="charge{i+3}"
+            /><br />
+            <span>DC</span>
+        </div>
+    {/each}
+
+    {#each isClickArr_wayfast as elm, i}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div
+            class="type {elm ? 'clicked' : ''}"
+            on:click={() => (isClickArr_wayfast[i] = !isClickArr_wayfast[i])}
+        >
+            <img
+                src="/img/charge_way{i}.png"
+                width="200"
+                height="120"
+                alt="charge_way{i}"
+            /><br />
+            <span>급속</span>
+        </div>
+    {/each}
+    {#each isClickArr_wayfull as elm, i}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div
+            class="type {elm ? 'clicked' : ''}"
+            on:click={() => (isClickArr_wayfull[i] = !isClickArr_wayfull[i])}
+        >
+            <img
+                src="/img/charge_way{i + 1}.png"
+                width="200"
+                height="120"
+                alt="charge_way{i + 1}"
+            /><br />
+            <span>완속</span>
+        </div>
+    {/each}
+</div>
+
+<!-- <table>
+    <tr>
+        <td colspan="2"><h1>타입</h1> </td>
+    </tr>
+    <tr>
+        <td>
+            <h1>급속</h1>
+            <br />
+            <img src="./img/fast.png" width="200" height="120" /></td
+        >
+
+        <td>
+            <h1>완속</h1>
+            <br /><img src="./img/full.png" width="200" height="120" /></td
+        >
+    </tr>
+</table> -->
+
+<style>
+    .container {
+        display: grid;
+        width: 100%;
+        max-width: 500px;
+        margin: auto;
+        grid-template-columns: 1fr 1fr;
+    }
+    .title {
+        text-align: center;
+        font-size: 2em;
+        font-weight: 700;
+        grid-column: span 2;
+    }
+    .clicked {
+        color: rgb(0, 0, 0);
+    }
+    .clicked > img {
+        background-color: rgb(123, 230, 57);
+    }
+</style>
---
+++ Evcharger/src/routes/server/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    let container:HTMLDivElement;
+
+    let map:kakao.maps.Map;
+
+    onMount(() => {
+        const options = {
+            //지도를 생성할 때 필요한 기본 옵션
+            center: new kakao.maps.LatLng(37.3194789, 126.838415), //지도의 중심좌표.  (사용자 현 위치로 바꿀 것)
+            level: 3, //지도의 레벨(확대, 축소 정도)  (확대 수준이 바뀔때마다 업데이트)
+        };
+        map = new kakao.maps.Map(container, options);
+    });
+</script>
+
+<!-- {#each Array(15) as _,index}`
+    <button on:click={() => map.setLevel(index)}>{index}로 레벨 바꾸기</button>
+{/each} -->
+
+<div id="map" bind:this={container} style="width:1536px;height:840px;" />
