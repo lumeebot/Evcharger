15cbde0ea4e325f025676894c3585e018bb58d89
Rumi <lumee.yi@gmail.com>
2023-07-22 14:57:15 +0900

    level adit

--- src/routes/server/+page.svelte
+++ src/routes/server/+page.svelte
@@ -3,6 +3,7 @@
     import { requestAPI } from "$lib/api";
     let container: HTMLDivElement;
 
+    let level = 3;
     let map: kakao.maps.Map;
     let latitude = 33.450701;
     let longitude = 126.570667;
@@ -30,14 +31,14 @@
         const options = {
             //지도를 생성할 때 필요한 기본 옵션
             center: new kakao.maps.LatLng(latitude, longitude), //지도의 중심좌표.  (사용자 현 위치로 바꿀 것)
-            level: 3, //지도의 레벨(확대, 축소 정도)  (확대 수준이 바뀔때마다 업데이트)
+            level: level, //지도의 레벨(확대, 축소 정도)  (확대 수준이 바뀔때마다 업데이트)
         };
         map = new kakao.maps.Map(container, options);
         console.log(map);
         getUserLocation();
-        const dom = await requestAPI({ pageNo: 1, numOfRows: 10, zcode:11});        // period: 5,
-        for(const t of dom.querySelectorAll('item')){
-            console.log(t.querySelector('lat')?.textContent)
+        const dom = await requestAPI({ pageNo: 1, numOfRows: 10, zcode: 11 }); // period: 5,
+        for (const t of dom.querySelectorAll("item")) {
+            console.log(t.querySelector("lat")?.textContent);
         }
     });
 </script>
@@ -63,12 +64,11 @@
         width: 70px;
         color: black;
         position: fixed;
-        top:0;
-        right:0;
-        z-index:2
-
+        top: 0;
+        right: 0;
+        z-index: 2;
     }
-    .bacolor{
+    .bacolor {
         background-color: whitesmoke;
     }
 </style>
