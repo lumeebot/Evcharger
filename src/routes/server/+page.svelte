b398ceec2dc88fc6d9a76c1833bcb093b046b5fa
Rumi <lumee.yi@gmail.com>
2023-07-30 14:50:31 +0900

    vfd

--- src/routes/server/+page.svelte
+++ src/routes/server/+page.svelte
@@ -9,6 +9,25 @@
     let map: kakao.maps.Map;
     let latitude = 33.450701;
     let longitude = 126.570667;
+
+    let positions = [
+        {
+            title: "카카오",
+            latlng: new kakao.maps.LatLng(33.450705, 126.570677),
+        },
+        {
+            title: "생태연못",
+            latlng: new kakao.maps.LatLng(33.450936, 126.569477),
+        },
+        {
+            title: "텃밭",
+            latlng: new kakao.maps.LatLng(33.450879, 126.56994),
+        },
+        {
+            title: "근린공원",
+            latlng: new kakao.maps.LatLng(33.451393, 126.570738),
+        },
+    ];
     function getUserLocation() {
         if (!navigator.geolocation) {
             throw "위치 정보가 지원되지 않습니다.";
