a7cdd3d2c6c4702db49fba48071f8a160371672b
Rumi <lumee.yi@gmail.com>
2023-08-27 12:51:06 +0900

    change option

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,15 +1,15 @@
-interface iRequestAPI{
-    pageNo:number;
-    numOfRows:number;
+interface iRequestAPI {
+    pageNo: number;
+    numOfRows: number;
     // period:number;
-    zcode:number;
+    zcode: number;
 }
 
-export const requestAPI = async (obj:iRequestAPI) => {
+export const requestAPI = async (obj: iRequestAPI) => {
     // let url = 'http://apis.data.go.kr/B552584/EvCharger/getChargerStatus'; /*URL*/
     const url = new URL('https://apis.data.go.kr/B552584/EvCharger/getChargerInfo');
     // 서비스키
-    url.searchParams.append('serviceKey', 'V1eEuRvqApUSfQZV0OD7hipwnjfd+sSgQvhboyJiuRP/7ioB0DMhINkxhkEylCgQjtOw+uwJ23mPCAEC5Gg8Xw==');  
+    url.searchParams.append('serviceKey', 'V1eEuRvqApUSfQZV0OD7hipwnjfd+sSgQvhboyJiuRP/7ioB0DMhINkxhkEylCgQjtOw+uwJ23mPCAEC5Gg8Xw==');
     url.searchParams.append('pageNo', String(obj.pageNo));
     url.searchParams.append('numOfRows', String(obj.numOfRows));
     // url.searchParams.append('period', String(obj.period));
