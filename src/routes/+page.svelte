7522fac5dd959b5263a384b5c7ed8443ead8b883
Rumi <lumee.yi@gmail.com>
2023-09-23 13:33:06 +0900

    변수 i 를 없앰, "타입"과 "선택완료" 버튼을 container에서 뺌

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts"  >
+<script lang="ts" >
     import { onMount } from "svelte";
     export let charger_type_DC = [false];
     export let charger_type_COMBO = [false];
@@ -6,81 +6,80 @@
     export let charger_type_AC3 = [false];
 </script>
 
+<div class="title">
+    <h1>타입</h1>
+</div>
 <div class="container">
-    <div class="title">타입</div>
-    {#each charger_type_AC3 as elm, i}
+    {#each charger_type_AC3 as elm}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div
             class="type {elm ? 'clicked' : ''}"
-            on:click={() =>
-                (charger_type_AC3[i] = !charger_type_AC3[i])}
+            on:click={() => (charger_type_AC3[0] = !charger_type_AC3[0])}
         >
             <img
-                src="/img/charge{i}.png"
+                src="/img/charge{0}.png"
                 width="200"
                 height="200"
-                alt="charge{i}"
+                alt="charge{0}"
                 style="cursor: pointer;"
             /><br />
             <span style="cursor: pointer;">AC3상</span>
         </div>
     {/each}
-    {#each charger_type_FULL as elm, i}
+    {#each charger_type_FULL as elm}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div
             class="type {elm ? 'clicked' : ''}"
-            on:click={() =>
-                (charger_type_FULL[i] = !charger_type_FULL[i])}
+            on:click={() => (charger_type_FULL[0] = !charger_type_FULL[0])}
         >
             <img
-                src="/img/charge{i + 1}.png"
+                src="/img/charge{1}.png"
                 width="200"
                 height="200"
-                alt="charge{i + 1}"
+                alt="charge{1}"
                 style="cursor: pointer;"
             /><br />
             <span style="cursor: pointer;">AC완속</span>
         </div>
     {/each}
-    {#each charger_type_COMBO as elm, i}
+    {#each charger_type_COMBO as elm}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div
             class="type {elm ? 'clicked' : ''}"
-            on:click={() =>
-                (charger_type_COMBO[i] = !charger_type_COMBO[i])}
+            on:click={() => (charger_type_COMBO[0] = !charger_type_COMBO[0])}
         >
             <img
-                src="/img/charge{i + 2}.png"
+                src="/img/charge{2}.png"
                 width="200"
                 height="200"
-                alt="charge{i + 2}"
+                alt="charge{2}"
                 style="cursor: pointer;"
             /><br />
             <span style="cursor: pointer;">DC콤보</span>
         </div>
     {/each}
-    {#each charger_type_DC as elm, i}
+    {#each charger_type_DC as elm}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div
             class="type {elm ? 'clicked' : ''}"
-            on:click={() =>
-                (charger_type_DC[i] = !charger_type_DC[i])}
+            on:click={() => (charger_type_DC[0] = !charger_type_DC[0])}
         >
             <img
-                src="/img/charge{i + 3}.png"
+                src="/img/charge{3}.png"
                 width="200"
                 height="200"
-                alt="charge{i + 3}"
+                alt="charge{3}"
                 style="cursor: pointer;"
             /><br />
             <span style="cursor: pointer;">DC</span>
         </div>
     {/each}
-
-    <div />
-    <a href="/server" style="cursor: pointer;" class="choose_finish">선택완료</a
-    >
 </div>
+<div class="choose_finish">
+    <button onclick="location.href='/server'">선택완료</button>
+    <!--a href="/server" style="cursor: pointer;" class="choose_finish">선택완료</a-->
+</div>
+
 
 <style>
     .container {
@@ -107,10 +106,16 @@
         font-weight: 500;
         font-size: xx-large;
         color: rgb(31, 155, 0);
+        text-align: center;
+    }
+    .choose_finish button {
+        font-size: xx-large;
+        background-color:rgb(107, 209, 158);
+        color: rgb(233, 22, 22)
     }
     span {
         font-weight: bold;
-        color: dimgray;
+        color: rgb(84, 145, 49);
     }
     a {
         text-decoration: none;
